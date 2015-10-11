4386b8eb1ebc3428bce812e9c923ca8d11cc6e11
James <james.king@berkeley.edu>
2015-10-11 10:49:45 -0600

    Added datalake loader

--- exercise_1/loading_and_modeling/load_data_lake.sh
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -1,2 +1,2 @@
-hadoop fs -mkdir dataLake
-hadoop fs -put /root/W205/workingData/*.csv 
+hadoop fs -mkdir /home/james/W205/W205/exercise_1/loading_and_modeling/datalake
+hadoop fs -put /home/james/W205/W205/workingData/*.csv /home/james/W205/W205/exercise_1/loading_and_modeling/dataLake
